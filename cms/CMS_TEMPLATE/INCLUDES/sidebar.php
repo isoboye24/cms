bf91050fbfff47910310aa3d8322cc40d96b82d0
isoboye24 <isobarguy4real@gmail.com>
2021-01-03 18:47:44 +0200

    I wrote a code for the search engine to work.

--- cms/CMS_TEMPLATE/INCLUDES/sidebar.php
+++ cms/CMS_TEMPLATE/INCLUDES/sidebar.php
@@ -1,16 +1,29 @@
 <div class="col-md-4">
+                
+     <?php
+        
+        if(isset($_POST['submit']))
+        {
+            $search = $_POST['search'];
+            echo $search;
+        }        
+    
+    ?>
 
                 <!-- Blog Search Well -->
                 <div class="well">
-                    <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
+                   <h4>Blog Search</h4>
+                   <form action="" method="post">                       
+                        <div class="input-group">
+                            <input name="search" type="text" class="form-control">
+                            <span class="input-group-btn">
+                                <button name="submit" class="btn btn-default" type="submit">
+                                    <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                            </span>
+                        </div>
+                   </form> <!--Search form-->
+                    
                     <!-- /.input-group -->
                 </div>
 
