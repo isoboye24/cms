66d5a0c9c4ea655821a7b693e84b6d93059b3098
isoboye24 <isobarguy4real@gmail.com>
2021-01-06 03:56:38 +0200

    I added table to the category page

--- cms/CMS_TEMPLATE/admin/categories.php
+++ cms/CMS_TEMPLATE/admin/categories.php
@@ -29,7 +29,26 @@
                                 </div>
                                 
                             </form>
-                        </div>
+                         </div> <!--   Add category form-->
+                        
+                        <div class="col-xs-6">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Category Title</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Baseball Category</td>
+                                        <td>Basketball Category</td>
+                                    </tr>
+                                    
+                                </tbody>
+                            </table>
+                        </div> <!--   Category table -->
+                        
                         
                         
                     </div>
