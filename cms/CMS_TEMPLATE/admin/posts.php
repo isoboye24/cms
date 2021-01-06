fd3ade6b1d7c0dc9b8b20ad72c2af355e46dc1a0
isoboye24 <isobarguy4real@gmail.com>
2021-01-07 01:23:45 +0200

    I created post page and added data

---
+++ cms/CMS_TEMPLATE/admin/posts.php
@@ -0,0 +1,96 @@
+<?php  include "INCLUDES/admin_header.php"; ?>
+<?php  include "functions.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php  include "INCLUDES/admin_navigation.php"; ?>
+        
+        
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to admin
+                            <small>Author</small>
+                        </h1>
+                        
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <td>Id</td>
+                                    <td>Title</td>
+                                    <td>Author</td>
+                                    <td>Category</td>
+                                    <td>Status</td>
+                                    <td>Image</td>
+                                    <td>Comments</td>
+                                    <td>Tags</td>                                    
+                                    <td>Date</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+<?php
+                                
+    //global $connection;
+    $query = "SELECT * FROM posts";
+    $select_posts = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_assoc($select_posts))
+    {
+        $post_id = $row['post_id'];
+        $post_category_id = $row['post_category_id'];
+        $post_title = $row['post_title'];
+        $post_author = $row['post_author'];
+        $post_date = $row['post_date'];
+        $post_image = $row['post_image'];
+        $post_tags = $row['post_tags'];
+        $post_comment_count = $row['post_comment_count'];
+        $post_status = $row['post_status'];
+        
+
+        echo "<tr>";
+        echo "<td>{$post_id}</td>";
+        echo "<td>{$post_title}</td>";
+        echo "<td>{$post_author}</td>";
+        echo "<td>{$post_category_id}</td>";
+        echo "<td>{$post_status}</td>";
+        echo "<td><img width = '40' src='images/{$post_image}' </td>";
+        echo "<td>{$post_comment_count}</td>";
+        echo "<td>{$post_tags}</td>";
+        echo "<td>{$post_date}</td>";
+                
+        echo "</tr>";
+    }         
+                                
+ ?>                                  
+                                   
+                                    <td>10</td>
+                                    <td>Bootstrap Framework</td>
+                                    <td>Isoboye Vincent</td>
+                                    <td>Bootstrap</td>
+                                    <td>Status</td>
+                                    <td>Image</td>
+                                    <td>comments</td>
+                                    <td>Tags</td>
+                                    <td>Date</td>
+                                
+                            </tbody>
+                        </table>
+                        
+                        
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+        
+<?php  include "INCLUDES/admin_footer.php"; ?>
