973740661d5a84743ad51ccb38c263aab582a477
isoboye24 <isobarguy4real@gmail.com>
2021-01-06 11:59:41 +0200

    I added code to delete selected category.

--- cms/CMS_TEMPLATE/admin/categories.php
+++ cms/CMS_TEMPLATE/admin/categories.php
@@ -63,13 +63,8 @@
                             </form>
                          </div> <!--   Add category form-->
                          
-                        <div class="col-xs-6">                                                   
-<?php
-                                        
-        $query = "SELECT * FROM categories";
-        $select_categories = mysqli_query($connection, $query);
-?>
- 
+                        <div class="col-xs-6"> <!--  category table -->                                                  
+
                             <table class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
@@ -78,8 +73,12 @@
                                     </tr>
                                 </thead>
                                 <tbody>                                   
-<?php
- 
+<?php 
+       //Find all categories
+                                 
+        $query = "SELECT * FROM categories";
+        $select_categories = mysqli_query($connection, $query);
+                                    
         while($row = mysqli_fetch_assoc($select_categories))
         {
             $cat_id = $row['cat_id'];
@@ -88,9 +87,25 @@
             echo "<tr>";
             echo "<td>{$cat_id}</td>";
             echo "<td>{$cat_title}</td>";
+            echo "<td> <a href='categories.php?delete={$cat_id}'> Delete </a></td>";
             echo "</tr>";
         }                       
-?>    
+?>
+
+<?php                                     
+   
+    //Delete category
+                                    
+    if(isset($_GET['delete']))
+    {
+        $the_cat_id = $_GET['delete'];
+        
+        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+        $delete_query = mysqli_query($connection, $query);
+        
+        header("location: categories.php"); //The refreshes the page to avoid double clicking before the above action takes place.
+    }
+ ?>                                   
                                    
                                 </tbody>
                             </table>
