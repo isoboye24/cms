ae12a4f35dd40849daa32db71f3ef3eb18849787
isoboye24 <isobarguy4real@gmail.com>
2021-01-09 03:40:07 +0200

    I put the categories in the dropdown as options to be connected to the posts table through the cat_id

--- cms/CMS_TEMPLATE/admin/INCLUDES/edit_post.php
+++ cms/CMS_TEMPLATE/admin/INCLUDES/edit_post.php
@@ -43,9 +43,22 @@
                 <select name="post_category" id="">
                 
                 
-<?php
-         
-    //the category codes
+<?php         
+    
+    $query = "SELECT * FROM categories ";
+    $select_categories = mysqli_query($connection, $query);
+                       
+    ConfirmQuery($select_categories);                   
+                       
+    while($row = mysqli_fetch_assoc($select_categories))
+    {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+        
+        echo "<option value='$cat_id'>{$cat_title}</option>";
+        
+    }
+
         
 ?>
              
