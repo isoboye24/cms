4bc41bc8bd6cae1a0189714134b45549582dfabb
isoboye24 <isobarguy4real@gmail.com>
2021-01-10 18:27:57 +0200

    Inserted new fields in the comment and made the comment engine work.

--- cms/CMS_TEMPLATE/post.php
+++ cms/CMS_TEMPLATE/post.php
@@ -61,18 +61,37 @@
     <?php 
         }
     ?>
+       
+<?php
+                
+     if(isset($_POST['create_comment']))
+     {
+         echo $_POST['comment_author'];
+     }
+                
+?>
+               
 
-        
-        <!-- Blog Comments -->
-
+               
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
+                    <form action="" method="post" role="form">
+                      <div class="form-group">
+                         <label for="author">Author</label>
+                          <input type="text" class="form-control" name="comment_author">
+                      </div>
+                       <div class="form-group">
+                         <label for="email">Email</label>
+                          <input type="email" class="form-control" name="comment_email">
+                      </div>
+                                              
                         <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                           <label for="Comment">Your Comment</label>
+                            <textarea name="comment_content" class="form-control" rows="3"></textarea>
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        
+                        <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
 
