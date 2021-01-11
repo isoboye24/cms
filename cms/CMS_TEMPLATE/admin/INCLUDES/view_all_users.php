3c28ecbc0fc4b79ec360ff9fb938f8623ab6e52f
isoboye24 <isobarguy4real@gmail.com>
2021-01-11 02:43:45 +0200

    I displayed users in admin

--- cms/CMS_TEMPLATE/admin/INCLUDES/view_all_users.php
+++ cms/CMS_TEMPLATE/admin/INCLUDES/view_all_users.php
@@ -6,56 +6,60 @@
                                     <th>Firstname</th>
                                     <th>Lastname</th>
                                     <th>Email</th>
-                                    <th>Role</th>
-                                    <th>Date</th>                                    
+                                    <th>Role</th>            
                                 </tr>
                             </thead>
                             <tbody>
 <?php
                                 
     global $connection;
-    $query = "SELECT * FROM comments";
-    $select_comments = mysqli_query($connection, $query);
+    $query = "SELECT * FROM users";
+    $select_users = mysqli_query($connection, $query);
                                 
-    if(!$select_comments)
+    if(!$select_users)
     {
         die("QUERY FAILED". mysqli_error($connection));
     }
 
-    while($row = mysqli_fetch_assoc($select_comments))
+    while($row = mysqli_fetch_assoc($select_users))
     {
-        $comment_id = $row['comment_id'];
-        $comment_post_id = $row['comment_post_id'];
-        $comment_author = $row['comment_author'];
-        $comment_content = $row['comment_content'];
-        $comment_email = $row['comment_email'];        
-        $comment_status = $row['comment_status'];
-        $comment_date = $row['comment_date'];
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_password = $row['user_password'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];        
+        $user_image = $row['user_image'];
+        $user_role = $row['user_role'];
+        
         
 
         echo "<tr>";
-        echo "<td>{$comment_id}</td>";
-        echo "<td>{$comment_author}</td>";
-        echo "<td>{$comment_content}</td>";
-        echo "<td>{$comment_email}</td>";
-        echo "<td>{$comment_status}</td>";
+        echo "<td>{$user_id}</td>";
+        echo "<td>{$username}</td>";
+        echo "<td>{$user_password}</td>";
+        echo "<td>{$user_firstname}</td>";
+        echo "<td>{$user_lastname}</td>";
+        echo "<td>{$user_email}</td>";
+        echo "<td>{$user_image}</td>";
+        echo "<td>{$user_role}</td>";
         
-        $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
-        $select_post_id_query = mysqli_query($connection, $query);
+//        $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
+//        $select_post_id_query = mysqli_query($connection, $query);
+//        
+//        while($row = mysqli_fetch_assoc($select_post_id_query))
+//        {
+//            $post_id = $row['post_id'];
+//            $post_title = $row['post_title'];
+//            
+//            echo "<td> <a href='../post.php?p_id=$post_id'>$post_title</a></td>";
+//        }
+//        
         
-        while($row = mysqli_fetch_assoc($select_post_id_query))
-        {
-            $post_id = $row['post_id'];
-            $post_title = $row['post_title'];
-            
-            echo "<td> <a href='../post.php?p_id=$post_id'>$post_title</a></td>";
-        }
-        
-        
-        echo "<td>{$comment_date}</td>";        
-        echo "<td><a href='comments.php?approve=$comment_id'>approve</a></td>";        
-        echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";        
-        echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";       
+              
+        echo "<td><a href='comments.php?approve='>approve</a></td>";        
+        echo "<td><a href='comments.php?unapprove='>Unapprove</a></td>";        
+        echo "<td><a href='comments.php?delete='>Delete</a></td>";       
         echo "</tr>";
     }         
                                 
