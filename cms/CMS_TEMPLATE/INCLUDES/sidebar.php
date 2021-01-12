67f891c2326a9fe2d7ada652a8148620ce259a3e
isoboye24 <isobarguy4real@gmail.com>
2021-01-12 03:31:21 +0200

    I created the login form in the sidebar

--- cms/CMS_TEMPLATE/INCLUDES/sidebar.php
+++ cms/CMS_TEMPLATE/INCLUDES/sidebar.php
@@ -22,6 +22,32 @@
         
         
         
+        <!-- Login -->
+        <div class="well">
+           <h4>Login</h4>
+               <form action="INCLUDE/login.php" method="post">                       
+                    <div class="form-group">
+                        <input name="username" type="text" class="form-control" placeholder="Enter Username">                        
+                    </div>
+                    <div class="input-group">
+                        <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                        <span class="input-group-btn">
+                           <button class="btn btn-primary" name="login" type="submit">Submit</button>
+                            
+                        </span>                        
+                    </div>                    
+               </form> <!--Search form-->
+
+            <!-- /.input-group -->
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
         
 
         <!-- Blog Categories Well -->
