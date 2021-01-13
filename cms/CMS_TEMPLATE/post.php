ac2d3491f1704565d7ab8e0835ad4427af0707ef
isoboye24 <isobarguy4real@gmail.com>
2021-01-13 02:49:25 +0200

    I removed the Read more button of the post.

--- cms/CMS_TEMPLATE/post.php
+++ cms/CMS_TEMPLATE/post.php
@@ -53,8 +53,7 @@
             <hr>
             <img class="img-responsive" src="admin/images/<?php echo $post_image; ?>" alt="Image not found">
             <hr>
-            <p><?php echo $post_content; ?></p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <p><?php echo $post_content; ?></p>            
 
             <hr>
         
