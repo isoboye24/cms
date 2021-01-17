feb6660a3ae3ded53713d29f7d7093982040ce57
isoboye24 <isobarguy4real@gmail.com>
2021-01-17 02:58:36 +0200

    I created a author_posts.php file and related all posts to their authors.

--- cms/CMS_TEMPLATE/author_posts.php
+++ cms/CMS_TEMPLATE/author_posts.php
@@ -13,26 +13,29 @@
             <div class="col-md-8">
                 
 <?php 
-
-    $query = "SELECT * FROM posts WHERE post_status = 'Published' ";
+                
+   if(isset($_GET['p_id']))
+   {
+       $the_post_id = $_GET['p_id'];
+       $the_post_author = $_GET['author'];
+   }
+                
+//Catching the post_id sent by the index.php file by using the superglobal GET
+    $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}' "; 
 
     $select_all_posts_query = mysqli_query($connection, $query);
 
     while($row = mysqli_fetch_assoc($select_all_posts_query))
     {
-        $post_id = $row['post_id'];
         $post_title = $row['post_title'];
         $post_author = $row['post_author'];
         $post_date = $row['post_date'];
         $post_image = $row['post_image'];
-        $post_content = substr($row['post_content'], 0, 100);
+        $post_content = $row['post_content'];
         $post_tags = $row['post_tags'];
         $post_comment_count = $row['post_comment_count'];
         $post_status = $row['post_status'];
-        
-        if($post_status == "Published")
-        {   
-       
+
 ?>
         
            <h1 class="page-header">
@@ -42,32 +45,26 @@
 
             <!-- First Blog Post -->
             <h2>
-<!--            The link is sending post_id to the post page which will catch the post_id and use it.-->
-                <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
+                <a href="#"><?php echo $post_title; ?></a>
             </h2>
             <p class="lead">
-                by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a>
+                All posts by <?php echo $post_author; ?>
             </p>
             <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
             <hr>
-            
-            <a href="post.php?p_id=<?php echo $post_id; ?>">
-                <img class="img-responsive" src="admin/images/<?php echo $post_image; ?>" alt="Image not found" width="400" height="400">
-            </a>
+            <img class="img-responsive" src="admin/images/<?php echo $post_image; ?>" alt="Image not found">
             <hr>
-            
-            <p><?php echo $post_content; ?></p>
-            <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <p><?php echo $post_content; ?></p>            
 
             <hr>
         
     <?php
-            }
+        
         }
     ?>
-
-        
-            </div>
+     
+     
+      </div>
 
             <!-- Blog Sidebar Widgets Column -->
  <?php include "INCLUDES/sidebar.php"; ?>           
