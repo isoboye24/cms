0fde6d1e07b38e808ae2f2d03df13095f1dc3b41
isoboye24 <isobarguy4real@gmail.com>
2021-01-19 03:29:00 +0200

    Instant users online count without refreshing...although not working for now because of the script.js file

--- cms/admin/js/script.js
+++ cms/admin/js/script.js
@@ -1,40 +1,33 @@
-tinymce.init({selector:'textarea'});
-
-$(document).ready(function(){
-   //Editor CKEditor              
-    ClassicEditor
-        .create( document.querySelector( '#body' ) )
-        .catch( error => {
-            console.error( error );
-        } );
-
-
-    // Rest of the code
-
-
-
-    $('#selectAllBoxes').click(function(event)
-    {
-        if(this.checked){
-            $('.checkBoxes').each(function(){
-                this.checked = true;
-            });
-        }
-       else{
-               $('.checkBoxes').each(function(){
-                this.checked = false;
-            });
-           }
-    });
-    
+//tinymce.init({selector:'textarea'});
  
-    
-    var div_box = "<div id='load-screen'><div id='loading'></div></div>";
-    $("body").prepend(div_box);
-    
-    $('#load-screen').delay(700).fadeOut(600, function(){
-        $this.remove();
+$(document).ready(function () {
+  //Editor CKEditor
+  ClassicEditor.create(document.querySelector('#body')).catch(error => {
+    console.error(error);
+  });
+ 
+  // Rest of the code
+ 
+  $('#selectAllBoxes').click(function (event) {
+    if (this.checked) {
+      $('.checkBoxes').each(function () {
+        this.checked = true;
+      });
+    } else {
+      $('.checkBoxes').each(function () {
+        this.checked = false;
+      });
+    }
+  });
+ 
+  var div_box = "<div id='load-screen'><div id='loading'></div></div>";
+  $('body').prepend(div_box);
+ 
+  $('#load-screen')
+    .delay(700)
+    .fadeOut(600, function () {
+      this.remove();
     });
-    
-    
 });
+
+
