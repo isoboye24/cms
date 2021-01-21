6d4249efa4d48655ded3f08aefdea9176a452789
isoboye24 <isobarguy4real@gmail.com>
2021-01-21 04:19:54 +0200

    I created a link for the comment count and sent a GET request. Also, I provided delete on the same page.

--- cms/admin/post_comments.php
+++ cms/admin/post_comments.php
@@ -1,5 +1,25 @@
-<?php 
+ <?php  include "INCLUDES/admin_header.php"; ?>
+<?php  include "functions.php"; ?>
 
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php  include "INCLUDES/admin_navigation.php"; ?>
+        
+        
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to comments
+                            <small>Author</small>
+                        </h1>
+                        
+ <?php                       
     if(isset($_POST['checkboxArray']))
     {
         foreach($_POST['checkboxArray'] as $post_value_id)
@@ -87,129 +107,126 @@
                            
                            <div class="col-xs-4">                               
                                <input type="submit" name="submit" class="btn btn-success" value="Apply">
-                               <a class="btn btn-primary" href="posts.php?source=add_post">Add New</a>                               
+                               <a class="btn btn-primary" href="posts.php?source=add_post">Add New</a>
                            </div>
-     
+  
+                           <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
-                                   <th><input type="checkbox" id="selectAllBoxes"></th>  
                                     <th>Id</th>
-                                    <th>Title</th>
                                     <th>Author</th>
-                                    <th>Category</th>
-                                    <th>Status</th>
-                                    <th>Image</th>
-                                    <th>Comments</th>
-                                    <th>Tags</th>                                    
+                                    <th>Comment</th>
+                                    <th>Email</th>
+                                    <th>Comment Status</th>
+                                    <th>In response to</th>
                                     <th>Date</th>
-                                    <th>View Post</th>
-                                    <th>Edit</th>
+                                    <th>Approve</th>                                    
+                                    <th>Unapprove</th>
                                     <th>Delete</th>
-                                    <th>Post Views Count</th>
                                 </tr>
                             </thead>
                             <tbody>
 <?php
                                 
     global $connection;
-    $query = "SELECT * FROM posts ORDER BY post_id DESC";
-    $select_posts = mysqli_query($connection, $query);
-    
-    ConfirmQuery($select_posts);
-    
-    while($row = mysqli_fetch_assoc($select_posts))
+    $query = "SELECT * FROM comments WHERE comment_post_id = ".mysqli_real_escape_string($connection, $_GET['id'])." ";
+    $select_comments = mysqli_query($connection, $query);
+                                
+    if(!$select_comments)
     {
-        $post_id = $row['post_id'];
-        $post_category_id = $row['post_category_id'];
-        $post_title = $row['post_title'];
-        $post_author = $row['post_author'];
-        $post_date = $row['post_date'];
-        $post_image = $row['post_image'];
-        $post_tags = $row['post_tags'];
-        $post_comment_count = $row['post_comment_count'];
-        $post_status = $row['post_status'];
-        $post_views_count = $row['post_views_count'];
-
-        echo "<tr>"; 
-?>
-             <td><input type='checkbox' class='checkBoxes' name="checkboxArray[]" value="<?php echo $post_id; ?>"></td>       
-               
-                             
-<?php
-       
-        echo "<td>{$post_id}</td>";
-        echo "<td>{$post_title}</td>";
-        echo "<td>{$post_author}</td>";
-        
-
-    $query = "SELECT * FROM categories WHERE cat_id = $post_category_id ";
-    $select_categories_id = mysqli_query($connection, $query);
-
-    while($row = mysqli_fetch_assoc($select_categories_id))
-    {
-        $cat_id = $row['cat_id'];
-        $cat_title = $row['cat_title'];
-
-        echo "<td>{$cat_title}</td>";
+        die("QUERY FAILED". mysqli_error($connection));
     }
 
-            echo "<td>{$post_status}</td>";
-            echo "<td><img width = '40' src='images/$post_image' </td>";
+    while($row = mysqli_fetch_assoc($select_comments))
+    {
+        $comment_id = $row['comment_id'];
+        $comment_post_id = $row['comment_post_id'];
+        $comment_author = $row['comment_author'];
+        $comment_content = $row['comment_content'];
+        $comment_email = $row['comment_email'];        
+        $comment_status = $row['comment_status'];
+        $comment_date = $row['comment_date'];
         
-            $query = "SELECT * FROM comments WHERE comment_post_id = $post_id ";
-            $send_comment_query = mysqli_query($connection, $query);
-            $row = mysqli_fetch_array($send_comment_query);            
-            $count_comments = mysqli_num_rows($send_comment_query);
-        
-            
-            echo "<td><a href='post_comments.php?id=$post_id'>$count_comments</a></td>";
-        
-        
-        
-        
-        
-            echo "<td>{$post_tags}</td>";
-            echo "<td>{$post_date}</td>";
-            echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
-            echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-            echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
-            echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
 
-            echo "</tr>";
+        echo "<tr>";
+        echo "<td>{$comment_id}</td>";
+        echo "<td>{$comment_author}</td>";
+        echo "<td>{$comment_content}</td>";
+        echo "<td>{$comment_email}</td>";
+        echo "<td>{$comment_status}</td>";
+        
+        $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
+        $select_post_id_query = mysqli_query($connection, $query);
+        
+        while($row = mysqli_fetch_assoc($select_post_id_query))
+        {
+            $post_id = $row['post_id'];
+            $post_title = $row['post_title'];
+            
+            echo "<td> <a href='../post.php?p_id=$post_id'>$post_title</a></td>";
+        }
+        
+        
+        echo "<td>{$comment_date}</td>";        
+        echo "<td><a href='comments.php?approve=$comment_id'>approve</a></td>";        
+        echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";        
+        echo "<td><a href='post_comments.php?delete=$comment_id&id=".$_GET['id']."'>Delete</a></td>";       
+        echo "</tr>";
     }         
                                 
  ?>                                  
-                             
+                                  
+                                
                             </tbody>
                         </table>
-                    </form>     
-                        
 <?php
 
-    if(isset($_GET['delete']))
-    {
-        $the_post_id = $_GET['delete'];
 
-        $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
+if(isset($_GET['approve']))
+{
+    $the_comment_id = $_GET['approve'];
+    
+    $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = $the_comment_id ";
+    
+    $unapprove_comment_query = mysqli_query($connection, $query);
+    header("Location: post_comments.php");
+}
 
-        $delete_query = mysqli_query($connection, $query);
-        header("Location: posts.php");
 
-    }
+if(isset($_GET['unapprove']))
+{
+    $the_comment_id = $_GET['unapprove'];
+    
+    $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id = $the_comment_id ";
+    
+    $unapprove_comment_query = mysqli_query($connection, $query);
+    header("Location: post_comments.php");
+}
 
-    if(isset($_GET['reset']))
-    {
-        $the_post_id = $_GET['reset'];
 
-        $query = "UPDATE posts SET post_views_count = 0 WHERE post_id = {$the_post_id}";
+if(isset($_GET['delete']))
+{
+    $the_comment_id = $_GET['delete'];
+    
+    $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id} ";
+    
+    $delete_query = mysqli_query($connection, $query);
+    header("Location: post_comments.php?id=".$_GET['id']."");
+}
 
-    //    To add escape
-    //    $query = "UPDATE posts SET post_views_count = 0 WHERE post_id =" .mysqli_real_escape_string($the_post_id)";
+?>  
+     
+                    </div>
+                </div>
+                <!-- /.row -->
 
-        $reset_query = mysqli_query($connection, $query);
-        header("Location: posts.php");
+            </div>
+            <!-- /.container-fluid -->
 
-    }
+        </div>
+        <!-- /#page-wrapper -->
+        
+    <?php  include "INCLUDES/admin_footer.php"; ?>
 
-?>                       
+                     
                         
