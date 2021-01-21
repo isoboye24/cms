ae054a9739a89e56a27c57141360f4df4fcac0d6
isoboye24 <isobarguy4real@gmail.com>
2021-01-21 02:30:37 +0200

    I cleaned up the edit and profile pages by removing unwanted information for users thereby upgrading the idea.

--- cms/admin/INCLUDES/edit_user.php
+++ cms/admin/INCLUDES/edit_user.php
@@ -6,7 +6,7 @@
     if(isset($_GET['p_id']))
     {
         $the_user_id = $_GET['p_id'];
-    }
+    
 
         //Take the data from the db where the id = id of the url
         $query = "SELECT * FROM users WHERE user_id = {$the_user_id} ";
@@ -19,57 +19,56 @@
             $username = $row['username']; // name of the select option below
             $user_lastname = $row['user_lastname'];
             $db_user_password = $row['user_password'];
-            $user_email = $row['user_email'];
-            $user_role = $row['user_role'];            
+            $user_email = $row['user_email'];                      
         }
 
-    if(isset($_POST['update_user']))
-    {
-         
-        $user_firstname = $_POST['user_firstname'];
-        $user_lastname = $_POST['user_lastname'];
-        $username = $_POST['username'];       
-        $user_password = $_POST['user_password'];       
-        $user_email = $_POST['user_email'];       
-                
-//        $post_image = $_FILES['image']['name'];
-//        $post_image_temp = $_FILES['image']['tmp_name'];        
-//        
-        $user_role = $_POST['user_role'];
-        
-         if(!empty($user_password))
-         {
-             $query = "SELECT user_password FROM users WHERE user_id = $the_user_id ";
-             $edit_user_query = mysqli_query($connection, $query);
-             ConfirmQuery($edit_user_query);
-             
-             $row = mysqli_fetch_array($edit_user_query);
-             
-             $db_user_password = $row['user_password'];
-         
-        
-        if($db_user_password != $user_password)
+        if(isset($_POST['update_user']))
         {
-             $hashed_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 12));
-        }
-           
-        //Edit and put the data into the db.
-        $query = "UPDATE users SET ";
-        $query .= "user_firstname = '{$user_firstname}', ";
-        $query .= "user_lastname = '{$user_lastname}', ";
-        $query .= "username = '{$username}', ";
-        $query .= "user_password = '{$hashed_password}', ";
-        $query .= "user_email = '{$user_email}', ";
-        $query .= "user_role = '{$user_role}' ";                 
-        $query .= "WHERE user_id = {$the_user_id} ";
-        
-        $update_user = mysqli_query($connection, $query);
 
-        ConfirmQuery($update_user);
-        
-        echo "User Updated ". "<a href='users.php'>View Users?</a>";
+            $user_firstname = $_POST['user_firstname'];
+            $user_lastname = $_POST['user_lastname'];
+            $username = $_POST['username'];       
+            $user_password = $_POST['user_password'];       
+            $user_email = $_POST['user_email'];       
+
+             if(!empty($user_password))
+             {
+                 $password_query = "SELECT user_password FROM users WHERE user_id = $the_user_id ";
+                 $edit_user_query = mysqli_query($connection, $password_query);
+                 ConfirmQuery($edit_user_query);
+
+                 $row = mysqli_fetch_array($edit_user_query);
+
+                 $db_user_password = $row['user_password'];
+
+
+                if($db_user_password != $user_password)
+                {
+                     $hashed_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 12));
+                }
+
+                //Edit and put the data into the db.
+                $query = "UPDATE users SET ";
+                $query .= "user_firstname = '{$user_firstname}', ";
+                $query .= "user_lastname = '{$user_lastname}', ";
+                $query .= "username = '{$username}', ";
+                $query .= "user_password = '{$hashed_password}', ";
+                $query .= "user_email = '{$user_email}', ";                                
+                $query .= "WHERE user_id = {$the_user_id} ";
+
+                $update_user = mysqli_query($connection, $query);
+
+                ConfirmQuery($update_user);
+                
+            }
+            echo "User Updated ". "<a href='users.php'>View Users?</a>";
         }
     }
+    else
+    {
+        header("Location: index.php");
+    }
+
 
 ?>
     
@@ -87,46 +86,7 @@
                 <label for="post_tags">Lastname</label>
                 <input type="text" class="form-control" name="user_lastname" value="<?php echo $user_lastname; ?>">
             </p>
-            
-<!--
-            <p class="form-group">
-                <label for="post_tags">Image</label>
-                <input type="file" class="form-control" name="user_image">
-            </p>
--->
-
-             
-             <p class="form-group">        
-                <select name="user_role" id="" class="form-control">
-                    <option value="<?php echo $user_role; ?>"><?php echo $user_role; ?></option>
-
-<?php
-
-    if($user_role == 'admin')
-    {
-        echo "<option value='Subscriber'>subscriber</option>";
-        echo "<option value='Collaborator'>collaborator</option>";
-    }
-    else if($user_role == 'subscriber')
-    {
-        echo "<option value='Admin'>admin</option>";
-        echo "<option value='Collaborator'>collaborator</option>";
-    }
-    else
-    {
-        echo "<option value='Subscriber'>subscriber</option>";
-        echo "<option value='Admin'>admin</option>";
-    }
-
-?>
-                    
-                    
-                    
-                    
-                                      
-                </select>          
-           </p>
-           
+       
             <p class="form-group">
                 <label for="author">Username</label>
                 <input type="text" class="form-control" name="username" value="<?php echo $username; ?>">
@@ -134,7 +94,7 @@
             
             <p class="form-group">
                 <label for="post_status">Password</label>
-                <input type="password" class="form-control" name="user_password" value="<?php echo $user_password; ?>">
+                <input type="password" class="form-control" name="user_password" autocomplete="off">
             </p>
             
             <p class="form-group">
--- cms/admin/profile.php
+++ cms/admin/profile.php
@@ -39,27 +39,6 @@ if(isset($_POST['update_user']))
         $user_password = $_POST['user_password'];       
         $user_email = $_POST['user_email'];       
                 
-//        $post_image = $_FILES['image']['name'];
-//        $post_image_temp = $_FILES['image']['tmp_name'];        
-//        
-        $user_role = $_POST['user_role'];
-        
-                
-//        Making the image stay when update post is clicked.
-//        move_uploaded_file($post_image_temp, "images/$post_image");
-        
-//        if(empty($post_image))
-//        {
-//            $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
-//            $select_image = mysqli_query($connection, $query);
-//            
-//            while($row = mysqli_fetch_array($select_image))
-//            {
-//                $post_image = $row['post_image'];
-//            }
-//            
-//        }
-//        
         
         //Edit and put the data into the db.
         $query = "UPDATE users SET ";
@@ -67,8 +46,7 @@ if(isset($_POST['update_user']))
         $query .= "user_lastname = '{$user_lastname}', ";
         $query .= "username = '{$username}', ";
         $query .= "user_password = '{$user_password}', ";
-        $query .= "user_email = '{$user_email}', ";
-        $query .= "user_role = '{$user_role}' ";                 
+        $query .= "user_email = '{$user_email}', ";             
         $query .= "WHERE username = '{$username}' ";
         
         $edit_user_query = mysqli_query($connection, $query);
@@ -76,13 +54,7 @@ if(isset($_POST['update_user']))
         ConfirmQuery($edit_user_query);         
     }
 
-
-
-
 ?>    
-          
-    
-    
 
     <div id="wrapper">
 
@@ -118,38 +90,6 @@ if(isset($_POST['update_user']))
                 <input type="text" class="form-control" name="user_lastname" value="<?php echo $user_lastname; ?>">
             </p>
             
-<!--
-            <p class="form-group">
-                <label for="post_tags">Image</label>
-                <input type="file" class="form-control" name="user_image">
-            </p>
--->
-
-             
-             <p class="form-group">        
-                <select name="user_role" id="">
-                    <option value="Subscriber"><?php echo $user_role; ?></option>
-
-<?php
-
-    if($user_role == 'admin')
-    {
-        echo "<option value='subscriber'>subscriber</option>";        
-    }    
-    else
-    {        
-        echo "<option value='admin'>admin</option>";
-    }
-
-?>
-                    
-                    
-                    
-                    
-                                      
-                </select>          
-           </p>
-           
             <p class="form-group">
                 <label for="author">Username</label>
                 <input type="text" class="form-control" name="username" value="<?php echo $username; ?>">
@@ -157,7 +97,7 @@ if(isset($_POST['update_user']))
             
             <p class="form-group">
                 <label for="post_status">Password</label>
-                <input type="password" class="form-control" name="user_password" value="<?php echo $user_password; ?>">
+                <input type="password" class="form-control" name="user_password" autocomplete="off">
             </p>
             
             <p class="form-group">
